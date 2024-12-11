7b7ecc599101c9b563a2b3beee500ca85ba6acd4
Jakub Milasz <jakubmilasz@gmail.com>
2024-12-11 18:44:32 +0100

    Some changes in animations.js

--- src/animations.js
+++ src/animations.js
@@ -1,14 +1,28 @@
-jQuery(document).ready(function(){
-
-  var myArray = ["a", "A", "i", "I", "o", "O", "u", "U", "w", "W", "z", "Z", "e", "E"];
-
-  for (var i=0; i < myArray.length; i++) {
-
-      var myArrayItem = (myArray[i]); 
-
-          jQuery('main').each(function() {
-              var text = jQuery(this).html();
-              jQuery(this).html(text.replace(new RegExp(' '+myArrayItem+' ', 'g') , ' '+myArrayItem+'&nbsp;'));
-          });
+class TextReplacer {
+  constructor(selector) {
+      // Wybieranie elementów na podstawie selektora
+      this.elements = document.querySelectorAll(selector);
+      this.myArray = ["a", "A", "i", "I", "o", "O", "u", "U", "w", "W", "z", "Z", "e", "E"];
+      this.replaceText();
   }
+
+  replaceText() {
+      this.elements.forEach(element => {
+          let text = element.innerHTML;
+
+          this.myArray.forEach(item => {
+              // Zastępowanie elementów w tekstach
+              const regex = new RegExp(` ${item} `, 'g'); 
+              text = text.replace(regex, ` ${item}&nbsp;`);
+          });
+
+          // Ustawianie przetworzonego tekstu z powrotem do HTML
+          element.innerHTML = text;
+      });
+  }
+}
+
+// Użycie komponentu dla wszystkich elementów 'main'
+document.addEventListener('DOMContentLoaded', () => {
+  new TextReplacer('main');
 });
