b32ed9fd6f5a28bf0e3c0ad33645d1a5c34aa531
Jakub Milasz <jakubmilasz@gmail.com>
2025-07-11 15:28:53 +0200

    Updating logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../img/logo.png"; // Import the logo image
+import logo from "../img/stopka.png"; // Import the logo image
 
 
 export default function Header() {
