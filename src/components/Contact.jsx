db6891031c06039fbe6118b56e932093ea93bd32
Jakub Milasz <jakubmilasz@gmail.com>
2025-12-24 01:08:04 +0100

    New layout for header and ofe content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -43,6 +43,8 @@ export default function Contact() {
               <p><strong>Telefon: </strong>783 273 664</p>
               <p><strong>E-mail: </strong>michal.milasz@adwokatura.pl</p>
               <p><strong>NIP: </strong>8692008386</p>
+              <p><strong>Adres</strong><br/>ul. Miodowa 26 lok. 1.4<br/>31-055 Kraków</p>
+              <p>Filia Kancelarii ul. Piłkarska 25<br/>32-800 Brzesko (Bucze)</p>
             </div>
           </div>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,38 @@
-import React from "react";
-import logo from "../img/stopka.png"; // Import the logo image
-
+import React from 'react';
+import logo from '../img/stopka.png'; // Import the logo image
 
 export default function Header() {
   return (
-    <header className="header">
-      <nav className="menu">
-        <div className="top-links">
-          <a href="home">Strona główna</a>
-          <a href="#specjalizacje">Specjalizacje</a>
-          <a href="o-mnie">O mnie</a>
-        </div>
-        <div className="bottom-link">
-          <a href="#kontakt">Kontakt</a>
-        </div>
+    <>
+      {/* Navbar jako osobny element - teraz będzie sticky na całej stronie */}
+      <nav className="navbar-container">
+        <ul className="nav-menu">
+          <li className="nav-item"><a href="home">STRONA GŁÓWNA</a></li>
+          <li className="nav-item"><a href="#specjalizacje">SPECJALIZACJE</a></li>
+          <li className="nav-item"><a href="o-mnie">O MNIE</a></li>
+          <li className="nav-item"><a href="#kontakt">KONTAKT</a></li>
+        </ul>
       </nav>
-      <div className="logo">
-        <img src={logo} alt="Logo" />
-      </div>
-    </header>
-  );
-}
 
+      {/* Sekcja Hero - tło i napisy */}
+      <header className="hero-section">
+        <div className="hero-column hero-left">
+          <div className="hero-text-wrapper">
+            <h1 className="hero-title">Zaufanie. Doświadczenie. Skuteczność.</h1>
+            <div className="fading-gold-line"></div>
+            <p className="hero-subtitle">
+              Reprezentujemy Klientów z pełnym zaangażowaniem,<br />
+              oferując kompleksową pomoc prawną.
+            </p>
+          </div>
+        </div>
+        
+        <div className="hero-column hero-right">
+          <div className="hero-logo-wrapper">
+            <img src={logo} alt="Kancelaria Michał Milasz" className="logo-img" />
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
