8bae4888832e650d60ccfb5b9ca7a106eae3dd49
Jakub Milasz <jakubmilasz@gmail.com>
2025-12-25 23:31:56 +0100

    Gradient update

--- src/components/Tiles.jsx
+++ src/components/Tiles.jsx
@@ -5,29 +5,31 @@ export default function Tiles() {
       <div className="heading" id="specjalizacje">
         <h2 className="heading-text">Specjalizacje</h2>
       </div>
-      <div className="banner">
-        <nav className="tiles">
-          <a className="tile" href="spadek">
-            Prawo spadkowe
-          </a>
-          <a className="tile" href="cywilne">
-            Prawo cywilne
-          </a>
-          <a className="tile" href="bankowe">
-            Kredyty i pożyczki
-          </a>
-          <a className="tile" href="gospodarcze">
-            Prawo gospodarcze
-          </a>
-          <a className="tile" href="przesyl">
-            Odszkodowania<br />za przesył
-          </a>
-          <a className="tile" href="ofe">
-            Zwrot pieniędzy<br />z OFE
-          </a>
-        </nav>
+      <div className="moving-background">
+        <div className="banner">
+          <nav className="tiles">
+            <a className="tile" href="spadek">
+              Prawo spadkowe
+            </a>
+            <a className="tile" href="cywilne">
+              Prawo cywilne
+            </a>
+            <a className="tile" href="bankowe">
+              Kredyty i pożyczki
+            </a>
+            <a className="tile" href="gospodarcze">
+              Prawo gospodarcze
+            </a>
+            <a className="tile" href="przesyl">
+              Odszkodowania<br />za przesył
+            </a>
+            <a className="tile" href="ofe">
+              Zwrot pieniędzy<br />z OFE
+            </a>
+          </nav>
+        </div>
+        <p className="spec-bottom">Zachęcamy do kontaktu także w sprawach spoza wskazanych wyżej dziedzin.</p>
       </div>
-      <p className="spec-bottom">Zachęcamy do kontaktu także w sprawach spoza wskazanych wyżej dziedzin.</p>
     </div>
     </>
   );
